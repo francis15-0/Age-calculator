21794cddcae702201d1dc1b639b115427265c98e
francis15-0 <nnadiekwemakuo.11@gmail.com>
2024-01-04 01:17:59 -0500

    Added the code for the javascript file

---
+++ age-calculator-app-main/scripts.js
@@ -0,0 +1,12 @@
+let submitBtn = document.getElementById("button");
+submitBtn.onclick = function(){
+    console.log("Hello");
+    let yearValue = document.getElementById("year").value;
+    let monthValue = document.getElementById("month");
+    let dayValue = document.getElementById("day");
+    console.log(yearValue);
+
+
+};
+
+console.log("hi");
